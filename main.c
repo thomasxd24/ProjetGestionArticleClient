e82938780969a3244ac2d1cce18ad96ff8cdfb46
Myogamevideo <alexandre.bougrat@hotmail.fr>
2019-01-08 13:04:28 +0100

    "tydzf"zt

--- main.c
+++ main.c
@@ -12,7 +12,6 @@ int main(void)
     printf("Chargement des Article...");
     nbArt = remplirTabArticle(tabArt, 100);
     printf("Réussi\n");
-    printf("cc");
     printf("Chargement des Client...");
     tabClient = chargeTabClient(tabClient,&nbClient);
     printf("Réussi\n");
