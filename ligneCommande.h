9cda1df6ea6dfdca97f9702686993567d4587c06
chtse <chtse@iutclinfa1908l.iut.local>
2019-01-23 09:22:38 +0100

    esemble en listecommande

--- ligneCommande.h
+++ ligneCommande.h
@@ -5,14 +5,22 @@
 #include"client.h"
 
 /*-------------------------------------------- Fonctions liste --------------------------------------------------------*/
+
 ListeCommande ensembleVide(void);
+
 ListeCommande insererEnTete(ListeCommande e, LigneCommande commande);
+
 ListeCommande ajouterCommande(ListeCommande e, LigneCommande commande);
+
 Booleen commandeEstVide(ListeCommande e);
+
 void afficherCommande(ListeCommande e);
+
 ListeCommande supprimerEnTete(ListeCommande e);
 ListeCommande supprimerCommande(ListeCommande e, int idCommande);
+
 int longueur(ListeCommande e);
+
 
 
 #endif 
